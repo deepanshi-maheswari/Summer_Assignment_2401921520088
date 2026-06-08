bd1cb590ed16fcf134bbfee1238d40fffbcfdeed
deepanshi-maheswari <deepanshimaheswari27@gmail.com>
2026-06-08 19:30:26 +0530

    Create Valid_Anagram.cpp

---
+++ week_2/Day_1/Valid_Anagram.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //method 1: bruteForce
+        // sort(s.begin() , s.end());
+        // sort(t.begin() , t.end());
+
+        // while(s.length() == t.length()){
+        //     if(s == t){
+        //         return true;
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+        // return false;
+
+        //method 2: counting
+        int freqTable[256] = {0};
+        for(int i = 0; i < s.size() ; i++){
+            freqTable[s[i]]++;
+        }
+
+        for(int i = 0; i < t.size(); i++){
+            freqTable[t[i]]--;
+        }
+
+        for(int i = 0; i < 256; i++){
+            if(freqTable[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
